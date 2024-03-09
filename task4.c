98e68fabe564d40555bf14ad050d080735ea3472
IacobVictoria <161374115+IacobVictoria@users.noreply.github.com>
2024-03-09 22:57:51 +0200

    Create task4.c

---
+++ task4.c
@@ -0,0 +1,397 @@
+
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h> 
+
+
+struct Imobil {
+    int cod;
+    char* adresa;
+    int nrEtaje;
+    float* preturi;
+};
+
+// Funcția pentru citirea unui obiect de tipul Imobil de la tastatură
+struct Imobil citesteImobil() {
+    struct Imobil imobil;
+    printf("Introduceti codul imobilului: ");
+    scanf("%d", &imobil.cod);
+
+    printf("Introduceti adresa imobilului: ");
+    getchar();
+    char buffer[100];
+    fgets(buffer, sizeof(buffer), stdin);
+
+    buffer[strcspn(buffer, "\n")] = '\0';
+    imobil.adresa = (char*)malloc(strlen(buffer) + 1);
+    strcpy(imobil.adresa, buffer);
+
+    printf("Introduceti numarul de etaje al imobilului: ");
+    scanf("%d", &imobil.nrEtaje);
+
+
+    imobil.preturi = (float*)malloc(imobil.nrEtaje * sizeof(float));
+
+    for (int i = 0; i < imobil.nrEtaje; ++i) {
+        printf("Introduceti pretul pentru etajul %d: ", i + 1);
+        scanf("%f", &imobil.preturi[i]);
+    }
+
+    return imobil;
+}
+
+// Funcția pentru calcularea mediei prețurilor dintr-un obiect de tipul Imobil
+float calculeazaMediePreturi(struct Imobil imobil) {
+    if (imobil.nrEtaje == 0 || imobil.preturi == NULL) {
+        return 0;
+    }
+
+    float suma = 0;
+    for (int i = 0; i < imobil.nrEtaje; i++) {
+        suma += imobil.preturi[i];
+    }
+
+    return suma / imobil.nrEtaje;
+}
+
+// Funcția pentru modificarea adresei unui obiect de tipul Imobil
+void modificaAdresa(struct Imobil* imobil, const char* nouaAdresa) {
+    free(imobil->adresa);
+    imobil->adresa = (char*)malloc(strlen(nouaAdresa) + 1);
+    strcpy(imobil->adresa, nouaAdresa);
+}
+
+// Funcția pentru afișarea informațiilor despre un obiect de tipul Imobil
+void afiseazaImobil(struct Imobil imobil) {
+    printf("Codul imobilului: %d\n", imobil.cod);
+    printf("Adresa imobilului: %s\n", imobil.adresa);
+    printf("Numarul de etaje al imobilului: %d\n", imobil.nrEtaje);
+    if (imobil.nrEtaje > 0 && imobil.preturi != NULL) {
+        printf("Preturile pe fiecare etaj: ");
+        for (int i = 0; i < imobil.nrEtaje; i++) {
+            printf("%.2f ", imobil.preturi[i]);
+        }
+        printf("\n");
+    }
+}
+//Functia care creeaza un nou vector
+struct Imobil* creeazaVector(struct Imobil* v, int nr, int* dim) {
+    struct Imobil* nou = (struct Imobil*)malloc(nr * sizeof(struct Imobil));
+    int k = 0;
+
+
+    for (int i = 0; i < nr; i++)
+    {
+        if (v[i].nrEtaje != 1 && v[i].cod != 90) {
+            nou[k] = v[i];
+            k++;
+        }
+    }
+    nou = (struct Imobil*)realloc(nou, k * sizeof(struct Imobil));
+    *dim = k;
+    return nou;
+}
+//Functia care concateneaza 2 vectori
+struct Imobil* concatenareVectori(struct Imobil* v, int nr, struct Imobil* vv, int nr1, int* dim) {
+    int d = nr + nr1;
+    struct Imobil* nou = (struct Imobil*)malloc(d * sizeof(struct Imobil));
+    int k = 0;
+
+
+    for (int i = 0; i < nr; i++)
+    {
+        nou[k] = v[i];
+        k++;
+
+    }
+    for (int i = 0; i < nr1; i++) {
+        nou[k] = vv[i];
+        k++;
+    }
+    nou = (struct Imobil*)realloc(nou, k * sizeof(struct Imobil));
+    *dim = k;
+    return nou;
+}
+//Functia afisare vector
+void afisareVector(struct Imobil* v, int nr) {
+    for (int i = 0; i < nr; i++) {
+        afiseazaImobil(v[i]);
+
+    }
+}
+
+
+//Creati un fisier in care sa aveti minim 10 obiecte de tipul structurii create.
+//  Asezarea in fisier a elementelor o faceti la libera alegere.
+void creareaFisier(int n) {
+    FILE* f;
+    f = fopen("imobile.txt", "w");
+    if (f == NULL) {
+        printf("Eroare deschidere");
+        return;
+    }
+    for (int i = 0; i < n; i++) {
+        struct Imobil imobil = citesteImobil();
+        fprintf(f, "%d\n%s\n%d\n", imobil.cod, imobil.adresa, imobil.nrEtaje);
+
+
+        for (int j = 0; j < imobil.nrEtaje; ++j) {
+            fprintf(f, "%.2f ", imobil.preturi[j]);
+        }
+        fprintf(f, "\n");
+    }
+    fclose(f);
+}
+
+
+//Scrieti intr-un program C functia care sa citeasca obiectele din fisier 
+// si sa le salveze intr-un vector.
+struct Imobil* citireFisierCreareVector(int* dim) {
+    FILE* f;
+    f = fopen("imobile.txt", "r");
+    if (f == NULL) {
+        printf("Nu se poate deschide fisierul.\n");
+        return NULL;
+    }
+
+    struct Imobil* vector = NULL;
+    int k = 0;
+    int cap = 10;
+    vector = (struct Imobil*)malloc(cap * sizeof(struct Imobil));
+
+
+    while (1) {
+        if (k >= cap) {
+
+            cap *= 2;
+            struct Imobil* temp = (struct Imobil*)realloc(vector, cap * sizeof(struct Imobil));
+            //Aceasta se face cu ajutorul funcției realloc. Realocarea nu doar mărește spațiul de memorie disponibil, ci și copiază datele existente în noul spațiu de memorie.
+            vector = temp;
+        }
+
+        struct Imobil imobil;
+        if (fscanf(f, "%d", &imobil.cod) != 1)break; //am while(1) tr sa ies candva asa ca fac !=1 care inseamna ca nu a reusit sa citeasca aia si va iesi cu break
+        fgetc(f);
+
+        char buffer[100];
+        fgets(buffer, sizeof(buffer), f);
+        buffer[strcspn(buffer, "\n")] = '\0';
+        imobil.adresa = (char*)malloc(strlen(buffer) + 1);
+
+        strcpy(imobil.adresa, buffer);
+
+        if (fscanf(f, "%d", &imobil.nrEtaje) != 1) break;
+        imobil.preturi = (float*)malloc(imobil.nrEtaje * sizeof(float));
+
+
+        for (int i = 0; i < imobil.nrEtaje; i++) {
+            fscanf(f, "%f", &imobil.preturi[i]);
+        }
+
+        vector[k++] = imobil;
+    }
+
+    *dim = k;
+    fclose(f);
+    return vector;
+}
+
+
+//salvare obiect intr-un fisier text
+
+void salvareImobilFisier(struct Imobil imobil) {
+    FILE* f;
+    f = fopen("imobile.txt", "a+"); //a+ to append to file
+    if (f == NULL) {
+        printf("Eroare");
+    }
+    fprintf(f, "%d\n%s\n%d\n", imobil.cod, imobil.adresa, imobil.nrEtaje);
+
+
+    for (int j = 0; j < imobil.nrEtaje; ++j) {
+        fprintf(f, "%.2f ", imobil.preturi[j]);
+    }
+    fprintf(f, "\n");
+}
+
+
+
+//salvare vector fisier text
+void salvareVectImobilFisier(struct Imobil* imobil, int nr) {
+    FILE* f;
+    f = fopen("imobile.txt", "a+"); //a+ to append to file
+    if (f == NULL) {
+        printf("Eroare");
+    }
+    for (int i = 0; i < nr; i++) {
+        fprintf(f, "%d\n%s\n%d\n", imobil[i].cod, imobil[i].adresa, imobil[i].nrEtaje);
+
+
+        for (int j = 0; j < imobil[i].nrEtaje; ++j) {
+            fprintf(f, "%.2f ", imobil[i].preturi[j]);
+        }
+        fprintf(f, "\n");
+    }
+}
+
+//matrice dezalocare
+struct Imobil copieImobil(struct Imobil c) {
+    struct Imobil nou;
+    nou.cod = c.cod;
+    nou.adresa = (char*)malloc(strlen(c.adresa) + 1);
+    strcpy(nou.adresa, c.adresa);
+    nou.nrEtaje = c.nrEtaje;
+    nou.preturi = (float*)malloc(sizeof(float) * c.nrEtaje);
+    for (int i = 0; i < c.nrEtaje; i++) {
+        nou.preturi[i] = c.preturi[i];
+    }
+    return nou;
+
+}
+int nrMinimEtaje(struct Imobil* v, int dim) {
+    int min = v[0].nrEtaje;
+    for (int i = 1; i < dim; i++) {
+        if (v[i].nrEtaje < min) {
+            min = v[i].nrEtaje;
+        }
+    }
+    return min;
+}
+int nrMaximEtaje(struct Imobil* v, int dim) {
+    int max = v[0].nrEtaje;
+    for (int i = 1; i < dim; i++) {
+        if (v[i].nrEtaje > max) {
+            max = v[i].nrEtaje;
+        }
+    }
+    return max;
+}
+//afisare matrice
+void afisareMatrice(int nrLinii, int* nrElementeLinie, struct Imobil** matrice) {
+    for (int i = 0; i < nrLinii; i++) {
+        for (int j = 0; j < nrElementeLinie[i]; j++) {
+            afiseazaImobil(matrice[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+
+    //Creati o functie care sa copieze (deep copy) elementele din vector intr-o matrice alocată dinamic.dupa un criteriu, dupa nr de etaje
+struct Imobil** creareMatricePeNrEtaje(struct Imobil* vector, int dim, int* nrLinii, int** nrElementeLinie) {
+    int etajeMin = nrMinimEtaje(vector, dim);
+    int etajemax = nrMaximEtaje(vector, dim);
+    *nrLinii = 2;
+    *nrElementeLinie = (int*)malloc(sizeof(int) * (*nrLinii));
+    struct Imobil** matrice = (struct Imobil**)malloc(sizeof(struct Imobil*) * (*nrLinii));
+    for (int i = 0; i < (*nrLinii); i++) {
+        (*nrElementeLinie)[i] = 0;                     //INITIALIZARE
+        matrice[i] = NULL;
+    }
+    for (int i = 0; i < dim; i++) {
+        int l = 0;
+        if (vector[i].nrEtaje == etajeMin) {
+            l = 0;
+        }
+        else if (vector[i].nrEtaje > etajeMin) {
+            l = 1;
+        }
+
+        struct Imobil* aux = (struct Imobil*)malloc(sizeof(struct Imobil) * ((*nrElementeLinie)[l] + 1));
+        if (matrice[l] != NULL) {
+            for (int i = 0; i < (*nrElementeLinie)[l]; i++) {
+                aux[i] = matrice[l][i];
+            
+            }
+            free(matrice[l]);
+        }
+
+        aux[(*nrElementeLinie)[l]] = copieImobil(vector[i]);
+        matrice[l] = aux;
+        ((*nrElementeLinie)[l])++;
+
+
+
+
+
+    }
+
+    return matrice;
+}
+//dezalocare matrice
+void dezalocareMatrice(struct Imobil*** matrice, int* nrLinii, int** nrElementeLinie) {
+    for (int i = 0; i < *nrLinii; i++) {
+        for (int j = 0; j < (*nrElementeLinie)[i]; j++) {
+            free((*matrice)[i][j].adresa);
+          
+
+        }
+        if ((*matrice)[i]) {
+            free((*matrice)[i]);
+        }
+    }
+    free(*matrice);
+    free(*nrElementeLinie);
+    *nrElementeLinie = NULL;
+    *nrLinii = 0;
+    *matrice = NULL;
+}
+
+void sortareCrescatoare(struct Imobil ** matrice, int nrLinii, int* nrElem) {
+    for (int i = 0; i < nrLinii - 1; i++) {
+        for (int j = i + 1; j < nrLinii; j++) {
+            if (nrElem[i] > nrElem[j]) {
+                int aux = nrElem[i];
+                nrElem[i] = nrElem[j];
+                nrElem[j] = aux;
+                struct Imobil* auxPointer = matrice[i];
+                matrice[i] = matrice[j];
+                matrice[i] = auxPointer;
+            }
+        }
+    }
+}
+int main() {
+    //struct Imobil i1 = citesteImobil();
+    //int nr = 0;
+    //struct Imobil* vector = (struct  Imobil*)malloc(nr * sizeof(struct Imobil));
+    //for (int i = 0; i < nr; ++i) {
+    //    printf("Introduceti informatiile pentru imobilul %d:\n", i + 1);
+    //    vector[i] = citesteImobil();
+    //}
+    //int dim = 0;
+    //int dim1 = 0;
+    //struct Imobil* nou = creeazaVector(vector, 0, &dim);
+    //struct Imobil* concat = concatenareVectori(vector, 0, vector, 0, &dim1);
+
+
+
+    //printf("\nInf Vector: ");
+    //afisareVector(concat, dim1);
+
+    ////  creareaFisier(10);
+    //  //vector din fisier
+    int dimenV = 0;
+    struct Imobil* vectorF = citireFisierCreareVector(&dimenV);
+    //afisareVector(vectorF, dimenV);
+
+    ////salvare imobil fisier
+    //salvareImobilFisier(i1);
+
+    ////salvare vector
+    //salvareVectImobilFisier(vectorF, dimenV);
+    //free(vector);
+    //free(nou);
+    //free(concat);
+
+
+    //MATRICE
+    int nrLinii = 0;
+    int* nrElementeLinie = NULL;
+    struct Imobil** imobile = creareMatricePeNrEtaje(vectorF, dimenV, &nrLinii, &nrElementeLinie);
+
+    afisareMatrice(nrLinii, nrElementeLinie, imobile);
+    return 0;
+}
